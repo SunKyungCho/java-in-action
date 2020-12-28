bfd9e218fc6241ed2ef6e0b8b931dca97729a62f
SunKyungCho <sunkyungch@gmail.com>
2020-12-28 21:58:25 +0900

    first commit

---
+++ java8-practice/src/test/java/me/study/java8practice/Java8PracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package me.study.java8practice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Java8PracticeApplicationTests {
+
+    @Test
+    @DisplayName("")
+    void mapTest() {
+
+
+    }
+
+}
